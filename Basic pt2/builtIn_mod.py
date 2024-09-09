0c75a4aa7d15aac7da3350fbf9fce58736ee438f
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2024-09-10 03:25:21 +0600

    Add files via upload

---
+++ Basic pt2/builtIn_mod.py
@@ -0,0 +1,13 @@
+from math import *
+from random import *
+from time import*
+result = ceil(4.0003)
+result=floor(3.4334)
+
+print(random())
+print(randint(1,100))
+
+sleep(2)
+print(choice(['sakib', 'mash', 'mush', 'rud', 'musta']))
+
+
