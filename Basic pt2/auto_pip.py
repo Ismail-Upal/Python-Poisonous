6027500fbe5d2bf983999d34f66e216076b11989
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2024-11-06 20:27:36 +0600

    Delete Basic pt2 directory

--- Basic pt2/auto_pip.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-from time import sleep
-
-sleep(5)
-for i in range(0,3):
-    pyautogui.write('hello world', interval=0.25)
-    pyautogui.press('enter')
-
-
--- Basic pt2/exception.py
+++
@@ -1,8 +0,0 @@
-try:
-    result =23/0
-except:
-    print('error happend')
-finally:
-    print('finally here')
-
-print('done')
