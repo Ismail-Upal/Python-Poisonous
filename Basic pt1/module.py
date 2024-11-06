452434f214cf8f17014afcc4ab372b9750b20e90
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2024-11-06 20:27:27 +0600

    Delete Basic pt1 directory

--- Basic pt1/module.py
+++
@@ -1,8 +0,0 @@
-from function import double_it as dt
-from kargs import famous_name as name
-# from deafult_args import *
-f_name=name('tomato', 'morich')
-print(f_name)
-result = dt(33)
-print(result)
-
